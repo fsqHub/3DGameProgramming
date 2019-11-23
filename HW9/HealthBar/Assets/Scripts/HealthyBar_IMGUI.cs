5bffefd403cdc0632470415924b1d473751b4410
fsq <1218424633@qq.com>
2019-11-23 16:29:10 +0800

    update details

--- HW9/HealthBar/Assets/Scripts/HealthyBar_IMGUI.cs
+++ HW9/HealthBar/Assets/Scripts/HealthyBar_IMGUI.cs
@@ -6,22 +6,25 @@ using UnityEngine.UI;
 public class HealthyBar_IMGUI : MonoBehaviour
 {
     
-	public float curHP;
-	public float lerpHP;
-	public float maxHP;
+	public float curHP;//当前血量
+	public float lerpHP;//变化后的目标血量
+	public float maxHP;//最大血量
 
-	public Rect HealthBar;
-	public Rect HealthUp;
-	public Rect HealthDown;
+	public Rect HealthBar;//血条框
+	public Rect HealthUp;//加血按钮
+	public Rect HealthDown;//减血按钮
 
 	public Slider HealthSlider;
     // Start is called before the first frame update
     void Start()
-    {
+    {   //相关变量初始化
+
+        //血量相关值初始化
         curHP = 0.1f;
         lerpHP = 0.1f;
         maxHP = 1.0f;
 
+        //条框，按钮位置初始化
         HealthBar = new Rect(25,25,300,50);
         HealthUp = new Rect(45,50,50,30);
         HealthDown = new Rect(250,50,50,30);
@@ -39,7 +42,10 @@ public class HealthyBar_IMGUI : MonoBehaviour
             lerpHP = lerpHP - 0.1f < 0.0f ? 0.0f : lerpHP -0.1f;
         }
 
+        //插值计算health值，实现血条平滑变化
         curHP = Mathf.Lerp(curHP,lerpHP,0.05f);
+
+        //用水平滚动条的宽度作为血条的显示值
         GUI.HorizontalScrollbar(HealthBar,0.0f,curHP,0.0f,maxHP);
     
         HealthSlider.value = curHP;
